00ca9fe0f6cece386732caf3d9cf1997c36a0028
nuuuwan <nuuuwan@gmail.com>
2026-06-25 12:24:13 +0530

    refactor: simplify data parsing in _build_high_risk_moh_areas_data method

--- src/dengue/ndcu_weekly/NDCUWeeklyHighRiskMOHAreasMixin.py
+++ src/dengue/ndcu_weekly/NDCUWeeklyHighRiskMOHAreasMixin.py
@@ -22,56 +22,55 @@ class NDCUWeeklyHighRiskMOHAreasMixin:
                     contents.append(content)
         return "\n".join(contents)
 
+    # flake8: noqa: C901
+    @staticmethod
+    def __parse_line(line, district_name):
+        tokens = line.split("\t")
+        if len(tokens) != 3:
+            return None, district_name
+        moh_area_name = tokens[0].strip().replace("*", "")
+
+        if (
+            not moh_area_name.strip()
+            or "province" in moh_area_name.lower()
+            or "total" in moh_area_name.lower()
+            or "moh area" in moh_area_name.lower()
+        ):
+            return None, district_name
+
+        if "district" in moh_area_name.lower():
+            district_name = (
+                moh_area_name.lower().replace("district", "").strip().title()
+            )
+            return None, district_name
+
+        if tokens[1] == "" and tokens[2] == "":
+            tokens0_tokens1 = tokens[0].split()
+            if len(tokens0_tokens1) >= 3:
+                moh_area_name = " ".join(tokens0_tokens1[:-2])
+                tokens[1] = tokens0_tokens1[-2]
+                tokens[2] = tokens0_tokens1[-1]
+
+        n_cases_last_week = Parse.int(tokens[1])
+        n_cases_this_week = Parse.int(tokens[2])
+        data = dict(
+            district_id=RegionUtils.get_region_id_from_name(district_name),
+            district_name=district_name,
+            moh_area_name=moh_area_name,
+            n_cases_last_week=n_cases_last_week,
+            n_cases_this_week=n_cases_this_week,
+        )
+        return data, district_name
+
     def _build_high_risk_moh_areas_data(self):
         content = self._get_high_risk_moh_areas_raw_content()
         data_list = []
 
         district_name = None
         for line in content.splitlines():
-            tokens = line.split("\t")
-            if len(tokens) != 3:
-                continue
-            moh_area_name = tokens[0].strip().replace("*", "")
-
-            if (
-                not moh_area_name.strip()
-                or "total" in moh_area_name.lower()
-                or "moh area" in moh_area_name.lower()
-            ):
-                continue
-
-            if "district" in moh_area_name.lower():
-                district_name = (
-                    moh_area_name.lower()
-                    .replace("district", "")
-                    .strip()
-                    .title()
-                )
-                continue
-
-            if "province" in moh_area_name.lower():
-
-                continue
-
-            if tokens[1] == "" and tokens[2] == "":
-                tokens0_tokens1 = tokens[0].split()
-                if len(tokens0_tokens1) >= 3:
-                    moh_area_name = " ".join(tokens0_tokens1[:-2])
-                    tokens[1] = tokens0_tokens1[-2]
-                    tokens[2] = tokens0_tokens1[-1]
-
-            n_cases_last_week = Parse.int(tokens[1])
-            n_cases_this_week = Parse.int(tokens[2])
-            data = dict(
-                district_id=RegionUtils.get_region_id_from_name(
-                    district_name
-                ),
-                district_name=district_name,
-                moh_area_name=moh_area_name,
-                n_cases_last_week=n_cases_last_week,
-                n_cases_this_week=n_cases_this_week,
-            )
-            data_list.append(data)
+            data, district_name = self.__parse_line(line, district_name)
+            if data is not None:
+                data_list.append(data)
 
         data_list.sort(
             key=lambda x: (str(x["district_id"]), x["moh_area_name"])
