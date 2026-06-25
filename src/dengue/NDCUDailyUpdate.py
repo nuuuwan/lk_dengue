c97ccac27f5d90f0d0a6ebac162cffff074a72cf
nuuuwan <nuuuwan@gmail.com>
2026-06-25 11:04:25 +0530

    feat: implement district data processing in NDCUDailyUpdate and add district_data.tsv

--- src/dengue/NDCUDailyUpdate.py
+++ src/dengue/NDCUDailyUpdate.py
@@ -1,5 +1,7 @@
+import os
+
 from dengue.ndcu_doc.NDCUDoc import NDCUDoc
-from utils_future import Log, TimeFormat
+from utils_future import Log, TimeFormat, TSVFile
 
 log = Log("NDCUDailyUpdate")
 
@@ -23,5 +25,51 @@ class NDCUDailyUpdate(NDCUDoc):
                 return date_str
         raise ValueError("Could not find date_str")
 
-    def build_custom_data(self):
-        pass
+    def _get_district_raw_tables(self) -> list[list[dict]]:
+        tables = []
+        for file_name in os.listdir(self.dir_tables):
+            if file_name.endswith(".tsv"):
+                table = TSVFile(
+                    os.path.join(self.dir_tables, file_name)
+                ).read()
+                content = str(table)
+                if "District" in content:
+                    tables.append(table)
+
+        return tables
+
+    @property
+    def district_data_file(self) -> str:
+        return TSVFile(os.path.join(self.dir_data, "district_data.tsv"))
+
+    def _build_district_data(self, force):
+        if self.district_data_file.exists and not force:
+            log.debug(f"{self.district_data_file} exists")
+            return
+
+        tables = self._get_district_raw_tables()
+        log.debug(f"Found {len(tables)} district tables")
+
+        data_list = []
+        for d in tables[0] + tables[1]:
+            district_name = None
+            n_cases = None
+            for k, v in d.items():
+                if "district" in k.lower():
+                    district_name = v
+                elif "cases" in k.lower():
+                    n_cases = int(v)
+            if district_name and n_cases is not None:
+                data = dict(
+                    district_name=district_name,
+                    n_cases=n_cases,
+                )
+                data_list.append(data)
+
+        self.district_data_file.write(data_list)
+        log.info(
+            f"Wrote {len(data_list)} rows to {self.district_data_file.path}"
+        )
+
+    def build_custom_data(self, force=False):
+        self._build_district_data(force)
--- src/dengue/ndcu_doc/NDCUDocRawTablesMixin.py
+++ src/dengue/ndcu_doc/NDCUDocRawTablesMixin.py
@@ -6,14 +6,14 @@ log = Log("NDCUDocRawTablesMixin")
 
 
 class NDCUDocRawTablesMixin:
+    @property
     def dir_tables(self) -> str:
         dir_tables = os.path.join(self.dir_data, "raw_tables")
         os.makedirs(dir_tables, exist_ok=True)
         return dir_tables
 
     def build_raw_tables(self, force=False):
-        dir_tables = self.dir_tables()
-        if os.path.exists(dir_tables) and not force:
-            log.debug(f"{dir_tables} exists")
+        if os.path.exists(self.dir_tables) and not force:
+            log.debug(f"{self.dir_tables} exists")
             return
-        self.pdf_file.build_raw_tables(dir_tables)
+        self.pdf_file.build_raw_tables(self.dir_tables)
