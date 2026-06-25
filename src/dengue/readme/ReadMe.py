7e34abea2451774db31f18bdccb0ebc0d728f11e
nuuuwan <nuuuwan@gmail.com>
2026-06-25 12:59:16 +0530

    feat: refactor get_lines_for_source_reports to use DOC_CLASS_LIST for better maintainability

--- src/dengue/readme/ReadMe.py
+++ src/dengue/readme/ReadMe.py
@@ -7,14 +7,15 @@ log = Log("ReadMe")
 
 class ReadMe:
     FILE = File("README.md")
+    DOC_CLASS_LIST = [NDCUDaily, NDCUWeekly]
 
     def get_lines_for_source_reports(self) -> list[str]:
         lines = [
             "## Source Reports",
             "",
         ]
-        lines.extend(NDCUDaily.get_lines_for_source_reports())
-        lines.extend(NDCUWeekly.get_lines_for_source_reports())
+        for doc_class in self.DOC_CLASS_LIST:
+            lines.extend(doc_class.get_lines_for_source_reports())
         return lines
 
     def build(self):
